51437192bd225e103c1c0d9f26dd70a09e3c2a32
Jinkouu <jonathan0174@live.com>
2023-12-02 21:53:26 +1100

    part 1 stopped using go

--- day2/main.go
+++ day2/main.go
@@ -5,13 +5,17 @@ import (
     "fmt"
     "log"
     "os"
+    "slices"
 )
 
 func main() {
-    fmt.Println("Hello, World!")
+    //fmt.Println("Hello, World!")
+    readFile()
 }
 
 func readFile(){
+    sliceOfWords := []string{}
+
     file, err := os.Open("C:\\Users\\jonat\\Documents\\GitHub\\AdventOfCode2023\\day2\\input.txt")
     if err != nil{
         log.Fatal(err)
@@ -22,6 +26,7 @@ func readFile(){
 
     for scanner.Scan(){
         fmt.Println(scanner.Text())
+        sliceOfWords.append(scanner.Text())
     }
 
     if err := scanner.Err(); err != nil {
---
+++ day2/main.py
@@ -0,0 +1,53 @@
+from collections import deque
+import re
+
+listOfWords = []
+listOfFixedWords = []
+listOfGames = []
+
+def readFile():
+    with open("day2\input.txt") as input:
+        for line in input:
+            listOfWords.append(line)
+
+def splitString():
+    for line in listOfWords:
+        text = re.split(': | *; ', line)
+        listOfFixedWords.append(text)
+        #print(text)
+
+def countColours():
+    count = 1
+    for word in listOfFixedWords:
+        dict = {"game" : 0, "red" : 0, "green": 0, "blue" : 0}
+        #print(word)
+        dict["game"] = count
+        for i in range (1, len(word)):
+            text = word[i].split(', ')
+            #print(text)
+            for j in range(0, len(text)):
+                num = text[j].split(' ')
+                #print(num)
+                if num[0].isnumeric() and int(num[0]) >= dict["green"] and "green" in num[1]:
+                    dict["green"] = int(num[0])
+                elif num[0].isnumeric() and int(num[0]) >= dict["red"] and "red" in num[1]:
+                    dict["red"] = int(num[0])
+                elif num[0].isnumeric() and int(num[0]) >= dict["blue"] and "blue" in num[1]:
+                    dict["blue"] = int(num[0])
+        #print(dict)
+        listOfGames.append(dict)
+        dict.values()
+        count += 1
+
+def findGame(red, green, blue):
+    sum = 0
+    for dict in listOfGames:
+        if dict["red"] <= red and dict["blue"] <= blue and dict["green"] <= green:
+            sum += dict["game"]
+            p#rint(dict)
+    return sum
+
+readFile()
+splitString()
+countColours()
+print(findGame(12, 13, 14))
