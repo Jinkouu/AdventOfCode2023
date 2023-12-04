b5a22a7e6da20cbdbecdf5ec0e89a83f32237fc2
Jinkouu <jonathan0174@live.com>
2023-12-04 13:15:26 +1100

    error for real input

--- day3/main.py
+++ day3/main.py
@@ -4,15 +4,16 @@ import re
 listOfChars = []
 listOfCoords = []
 symbols = {}
+parts = {}
 currNum = ""
 
 
 def readFile():
-    with open("day3\\test.txt") as input:
+    with open("day3\\input.txt") as input:
         for lines in input:
             newLine = lines.strip()
             listOfChars.append([(line) for line in newLine])
-    #print(listOfChars)
+    print(listOfChars)
 
 def findSymbols():
     for row in range(len(listOfChars)):
@@ -41,22 +42,41 @@ def findInts():
 
 def searchAround():
     for element in listOfCoords:
+        height = len(listOfCoords) - 1
+        width = len(listOfCoords) if listOfCoords else 0
         #start = element[0]
         startX = element[0][0]
         startY = element[0][1]
         #end = element[1]
         endX = element[1][0]
         endY = element[1][1]
+        #print(startX, startY, endX, endY)
+        #print(height, width)
         if (startX - 1) > 0: #if left isnt out of bounds, increase search area
             startX -= 1
         if (startY - 1) > 0: #if top isnt out of bounds, increase search area
             startY -= 1
-        if (endX + 1) < len(listOfCoords[0]): # if right isnt out of bounds, increase search area
+        if (endX + 1) < (width): # if right isnt out of bounds, increase search area
             endX += 1
-        if (endY + 1) < len(listOfCoords): # if bottom isnt out of bounds, increase search area
+        if (endY + 1) < (height): # if bottom isnt out of bounds, increase search area
             endY += 1
-        
-        
+        #print(startX, startY, endX, endY)
+        for row in range (startX, endX+1):
+            for col in range(startY, endY+1):
+                #print(row, col)
+                if listOfChars[row][col] in symbols.keys():
+                    #print(listOfChars[row][col])
+                    if element[2] not in parts:
+                        parts[element[2]] = 1
+                    else:
+                        parts[element[2]] += 1
+    print(parts)
+
+def sumOfParts():
+    sum = 0
+    for key in parts:
+        sum += int(key)
+    print(sum)
 
 
 def prints():
@@ -72,5 +92,6 @@ readFile()
 findSymbols()
 findInts()
 searchAround()
+sumOfParts()
 #prints()
 
