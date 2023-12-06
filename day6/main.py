4a4f6f9be255211180d4af9b356be9574a99a986
Jinkouu <jonathan0174@live.com>
2023-12-06 22:48:30 +1100

    part 2 complete

--- day6/main.py
+++ day6/main.py
@@ -2,7 +2,7 @@ time = []
 dist = []
 ways = []
 
-def readFile():
+def readFilePart1():
     with open("day6\\input.txt") as input:
         for lines in input:
             newLine = lines.strip()
@@ -18,7 +18,7 @@ def readFile():
                 for i in range(1, len(newLine)):
                     dist.append(newLine[i])
 
-def calc():
+def calcPart1():
     for race in range (len(time)):
         numOfWays = 0
         for hold in range (int(time[race])):
@@ -31,5 +31,22 @@ def calc():
         multi *= way
     print(multi)
 
-readFile()
-calc()
+def calcPart2():
+    timing = ""
+    distance = ""
+    for t in time:
+        timing += str(t)
+    for d in dist:
+        distance += str(d)
+    #print (timing)
+
+    numOfWays = 0
+    for hold in range (int(timing)):
+        if(hold * (int(timing) - hold) > int(distance)):
+            #print(hold)
+            numOfWays += 1
+    print(numOfWays)
+
+readFilePart1()
+calcPart1()
+calcPart2()
