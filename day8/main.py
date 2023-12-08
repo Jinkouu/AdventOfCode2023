255dd234bd4f0caf573eb3bd65b19d1664437450
Jinkouu <jonathan0174@live.com>
2023-12-09 01:17:51 +1100

    complete

--- day8/main.py
+++ day8/main.py
@@ -1,3 +1,4 @@
+from math import lcm
 inp = ""
 traversal = {}
 
@@ -31,8 +32,7 @@ def readFile():
 #            return route
 #    return
 
-def calcSteps(currentKey):
-    
+def calcSteps1(currentKey):
     current = traversal[currentKey]
     steps = 0
     finished = False
@@ -54,40 +54,41 @@ def calcSteps(currentKey):
                 finished = True
                 return steps
 
+def calcSteps(currentKey):
+    current = traversal[currentKey]
+    steps = 0
+    finished = False
+    #print(current)
+    while finished == False:
+        for direction in inp: # for each letter in INP (L or R)
+            if "L" in direction:
+                #current = getLeft(current)
+                currentKey = traversal[currentKey][0]
+                current = traversal[currentKey]
+                steps += 1
+            else:
+                currentKey = traversal[currentKey][1]
+                current = traversal[currentKey]
+                steps += 1
+
+            #also check if it is at the destination (ZZZ)
+            if currentKey[2] == "Z":
+                finished = True
+                return steps
+
 def calcSteps2():
     startPoints = []
     for key in traversal.keys():
         if key[2] == "A":
             startPoints.append(key)
-    print(startPoints)
-
-    finished = False
-    steps = 0
-
-    while finished == False:
-        for direction in inp:
-            temp = []
-            for key in startPoints:
-                #print(key)
-                if "L" in direction:
-                    newKey = traversal[key][0]
-                else:
-                    newKey = traversal[key][1]
-                temp.append(newKey)
-            startPoints = temp
-            steps += 1
-            if all(key[2] == "Z" for key in startPoints):
-                return steps
-
-
-
-
     
+    result = []
+    for key in startPoints:
+        result.append(calcSteps(key))
+    return lcm(*result)
 
 
 readFile()
-#print(inp)
-#print(traversal)
-#print(calcSteps("GNK"))
-print(calcSteps2())
+print("Part 1:", calcSteps("GNK"))
+print("Part 2:", calcSteps2())
             
