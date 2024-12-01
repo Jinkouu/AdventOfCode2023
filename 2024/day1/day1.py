2abbe6d0693a6e9bb2743dc489425919feb36342
Jinkouu <jonathan0174@live.com>
2024-12-01 20:09:23 +1100

    part 2, works on test

--- 2024/day1/day1.py
+++ 2024/day1/day1.py
@@ -28,5 +28,27 @@ def calcDiff():
         diff += abs(int(list1[i]) - int(list2[i]))
     print(diff)
 
+def dictionarise():
+    dict1 = {key: 0 for key in list1}
+    dict2 = {key: 0 for key in list2}
+
+    for n in list1:
+        dict1[n] += 1
+    for n in list2:
+        dict2[n] += 1
+    
+    keys1 = set(dict1.keys())
+    keys2 = set(dict2.keys())
+
+    common_keys = keys1 & keys2
+
+    sim = 0
+    for num in common_keys:
+        sim += dict1[num] * int(num) * dict2[num] 
+    print(sim)
+        
+
+
 readFile()
 calcDiff()
+dictionarise()
