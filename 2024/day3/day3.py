b0127595428eb87de2b6d9a058b94bb8c68f9b3d
Jinkouu <jonathan0174@live.com>
2024-12-03 23:35:26 +1100

    day 3, part 1 works on test

---
+++ 2024/day3/day3.py
@@ -0,0 +1,50 @@
+def readFile():
+    with open("test.txt") as input:
+        input = input.read()
+        #print(input)
+        results = []
+
+        index = 0
+        while index < len(input):
+            index = input.find("mul", index) # sets the index as the instance of the next "mul"
+            if index == -1: #if there is no more, break
+                break
+        
+            snippet = input[index + 3:index + 12] #get mul + 9 characters afterwards (upto 6 digits and 3 symbols)
+            results.append(snippet)
+
+            index += 3
+    multResult = 0
+    for expression in results:
+        #for i in range (0, len(expression)):
+        i = 0
+        while i < len(expression):
+            if expression[i] == "(":
+                j = i + 1
+                digits1 = ""
+                while j < len(expression) and expression[j].isdigit() and j-1 <= 3: #finds up to 3 digits
+                    digits1 += expression[j]
+                    j += 1
+
+                if j < len(expression) and expression[j] == ",": #finds comma
+                    j += 1
+                else:
+                    break
+
+                k = j
+                digits2 = ""
+                while k < len(expression) and expression[k].isdigit() and k-j <= 3: #finds up to 3 digits
+                    digits2 += expression[k]
+                    k += 1
+
+                if k < len(expression) and expression[k] == ')': # Valid '(digits,digits)', remove '('
+                    multResult = multResult + (int(digits1) * int(digits2))
+                    break
+                else:
+                    break
+            else:
+            # Ignore characters outside of parentheses
+                i += 1
+    print(multResult)
+    #print(results)
+readFile()
